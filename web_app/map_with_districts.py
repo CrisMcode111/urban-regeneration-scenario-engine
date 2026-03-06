fa8dc73e03dcf41f9f1ee41474e8bf371fa9386c
Zahra Rauf <zahra.aleem@gmail.com>
2026-03-06 10:17:55 +0100

    the proposed scenarios with fit scores incorporated on the map

--- web_app/map_with_districts.py
+++ web_app/map_with_districts.py
@@ -1,25 +1,64 @@
 import folium
 import geopandas as gpd
+import json
 
 # Load district boundaries
-gdf = gpd.read_file("montgomery_districts.geojson")
-print(gdf[["District","Name"]])
+districts = gpd.read_file("montgomery_districts.geojson")
 
-data = gpd.read_csv("data/district_data.csv")
 
-gdf = gdf.merge(data, on="DISTRICT")
+# load scenario file
+with open("../data/scenario_validation.json", "r") as f:
+    scenarios = json.load(f)
+
+# Convert JSON to dictionary
+scenario_dict = {d["district"]: d for d in scenarios}
 
 # Center map roughly on Montgomery
 m = folium.Map(location=[32.37, -86.30], zoom_start=11)
 
-# Add all districts at once
-folium.GeoJson(
-    gdf,
-    name="Districts",
-    tooltip=folium.GeoJsonTooltip(
-        fields=["District","Name"],
-        aliases=["District:", "Council Member:"]
-    )
-).add_to(m)
+# function for styling the districts(optional)
+def style_function(feature):
+    return {
+        "fillColor": "#3186cc",
+        "color": "black",
+        "weight": 1,
+        "fillOpacity": 0.4
+    }
+
+for _, row in districts.iterrows():
+
+    district_id = str(row["District"]).split()[-1]   # extract 1,2,3...
+
+    scenario_text = ""
+
+    if district_id in scenario_dict:
+        district_data = scenario_dict[district_id]
+
+        for s in district_data["scenario"]:
+            actions = "<br>".join(s["actions"])
+
+            scenario_text += f"""
+            <b>{s['scenario_type']}</b><br>
+            {s['title']} (score: {s['fit_score']})<br>
+            {actions}<br><br>
+            """
+
+    popup_html = f"""
+    <h4>District {district_id}</h4>
+    {scenario_text}
+    """
+
+    folium.GeoJson(
+        row["geometry"],
+        tooltip=f"District {district_id}",
+        popup=folium.Popup(popup_html, max_width=400),
+        style_function=lambda x:{
+            "fillColor":"blue",
+            "color":"black",
+            "weight":1,
+            "fillOpacity":0.4
+        }
+    ).add_to(m)
+
 
 m.save("montgomery_all_districts_map.html")
